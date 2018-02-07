425f20179cd4a89606515099d017ffb9f1f42d3e
takahara kazuma <takahara.kazuma@arl.sys.es.osaka-u.ac.jp>
2018-02-07 23:30:13 +0900

    Import standard utilities in __init__.py

---
+++ kzm4269/__init__.py
@@ -0,0 +1,9 @@
+from . import argtypes
+from . import exc_utils as exc
+from . import file_utils as file
+from . import func_utils as func
+from . import imp_utils as imp
+from . import logging_utils as logging
+from . import mp_utils as mp
+from . import os_utils as os
+from . import str_utils as str
